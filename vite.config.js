679119212e3ce078aea1f75d06346741d37af9e1
arland17 <muhammad.arlanda@student.umn.ac.id>
2024-12-08 14:36:22 +0700

    update vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+// Vite configuration
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js'], // Ensure correct paths
             refresh: true,
         }),
     ],
+    server: {
+        hmr: {
+            protocol: 'ws', // Use WebSockets for hot module replacement (HMR)
+            host: 'localhost', // Localhost for development
+        },
+    },
 });
