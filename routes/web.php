d30be3534c009fa18b1252c6bf8c6e064251a8b0
arland17 <muhammad.arlanda@student.umn.ac.id>
2024-12-08 16:53:35 +0700

    done!

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RecommendationController;
 
+// Menampilkan halaman home di root URL
+Route::get('/', function () {
+    return view('home');
+});
+
 Route::post('/recommendation', [RecommendationController::class, 'getRecommendation'])->name('recommendation.get');
 
 Route::get('/home', function () {
@@ -13,6 +18,6 @@ Route::get('/alurkerja', function () {
     return view('alurkerja');
 });
 
-Route::get('/test', function() {
+Route::get('/test', function () {
     return view('test');
 });
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-// Vite configuration
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'], // Ensure correct paths
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
-    server: {
-        hmr: {
-            protocol: 'ws', // Use WebSockets for hot module replacement (HMR)
-            host: 'localhost', // Localhost for development
-        },
-    },
 });
